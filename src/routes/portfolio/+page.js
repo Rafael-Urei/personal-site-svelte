f2764e285d4fde68c76cc630e30f3933086cf983
Rafael-Urei <urei.dev@outlook.com>
2023-12-18 10:14:04 -0300

    Starting optmizing images

--- src/routes/portfolio/+page.js
+++ src/routes/portfolio/+page.js
@@ -1,7 +1,7 @@
 import { dev } from '$app/environment';
 
 export async function load({fetch}) {
-    const res = await fetch(`https://gist.githubusercontent.com/Rafael-Urei/1b65db476c98b3c9e4c3886acec8ed05/raw/de2fcce88ea10368e5d3afd1db341ce915ccc851/projects.json`)
+    const res = await fetch(`https://gist.githubusercontent.com/Rafael-Urei/94b6c4de4a94e10d2696ff3d4f438a2c/raw/50d6aac1ed5d31016dd40ae09d3c31faf6ba5e3a/projects.json`)
     const projects = await res.json();
 
     return {
